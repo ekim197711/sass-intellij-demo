ba37323cbe71896ca4996a9fc3af02aad1b06f5a
Mike Møller Nielsen <mmni@protonmail.com>
2020-07-25 14:28:42 +0200

    website

---
+++ initgit.sh
@@ -0,0 +1,6 @@
+echo "# sass-intellij-demo" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin git@github.com:ekim197711/sass-intellij-demo.git
+git push -u origin master
